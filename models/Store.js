1b9055189f6a2a8b5117197d9cdffb53fd8a0c62
Manuel Gonzalez <manuelmaingonzalez@gus.global>
2020-07-03 12:24:16 +0200

    Updated reset pass mail sending && locking down the application

--- models/Store.js
+++ models/Store.js
@@ -33,6 +33,11 @@ const storeSchema = new mongoose.Schema({
   },
 
   photo: String,
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: 'You must supplu an author'
+  }
 });
 
 storeSchema.pre("save", async function (next) {
